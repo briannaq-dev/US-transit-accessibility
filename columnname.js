6ea56da856845209dc5af3df653435192b19dd1d
Zahra Wibisana <wibisana.z@northeastern.edu>
2025-11-07 14:34:18 -0500

    Updated columns in column table

--- columnname.js
+++ columnname.js
@@ -1,8 +1,8 @@
 const columns = [
-    { name: "state", description: "Name of the U.S. state" },
-    { name: "unemployment_rate", description: "Percentage of people unemployed in the labor force" },
-    { name: "transit_access", description: "Level of public transit accessibility within the state" },
-    { name: "median_income", description: "Median household income in USD" }
+    { name: "CBSA_Name", description: "Name of the core based statistical area (or metropolitan region) in which block group resides." },
+    { name: "TrAccess_Index", description: "An index of relative accessibilitycompared to other block groups within the same metro region, as measured by travel time to working-age population via transit." },
+    { name: "Pop_byTr", description: "Total population able to access the block group within a 45-minute transit and walking commute" },
+    { name: "Pop_byTr_min", description: "Minimum Pop_byTr among all block groups within same CBSA" }
 ];
 
 const container = document.getElementById("column-table");
